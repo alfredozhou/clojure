404dfd77f143faf1b2425e137be0e1e2b1667a50
salsayarroz <salsayarroz@gmail.com>
2011-01-03 18:05:24 -0500

    broken thing to work at home

--- karatechop2/src/kata/main.clj
+++ karatechop2/src/kata/main.clj
@@ -1,27 +1,48 @@
-(ns kata.main)
+(ns kata.main
+  (:use clojure.contrib.math))
 
 (defn first-half [array]
   (take (/ (count array) 2) array)
   )
 
-(defn second-half [array]
-  (drop (/ (count array) 2) array)
+(defn i-am-in-second-half []
+  (print "I am in second half\n")
+  :second_half
   )
 
 (defn which-half? [array target]
+  (print "target=" target " sub list ="  array "\n")
   (let [first-half (first-half array)]
     (cond
-      (>= (last first-half) target) first-half
-      :else (second-half array))
+      (>= (last first-half) target) :first_half
+      :else (i-am-in-second-half))
     ))
 
-(defn where-is-it? [array target]
-  (let [section (which-half? array target)]
-    (cond
-      (= (count section) 0) nil
-      (and (= (count section) 1) (= (first section) target)) :true
-      :else (where-is-it? section target))
+
+(defn sub-array [array from to]
+  (subvec array from to)
+  )
+
+(defn calculate-index [to from]
+  (+ (/ to 2) (/ (- to from) 2))
+  )
+
+(defn- private-where-is-it? [array target from-index to-index]
+  ;  (print "target = " target " list =" array " from " from-index " to " to-index "\n")
+  (cond
+    (and (= from-index to-index) (= (nth array to-index) target)) to-index
+    (and (= from-index to-index) (not= (nth array to-index) target)) nil
+    :else
+    (let [which-half? (which-half? (sub-array array from-index to-index) target)]
+      (cond
+        (= which-half? :first_half) (private-where-is-it? array target from-index (floor (/ to-index 2)))
+        :else (private-where-is-it? array target (calculate-index  to-index from-index) to-index)
+        ))
     )
+  )
+
+(defn where-is-it? [array target]
+  (private-where-is-it? array target 0 (count array))
   )
   
 
--- karatechop2/test/kata/test/main.clj
+++ karatechop2/test/kata/test/main.clj
@@ -3,18 +3,18 @@
   (:use [clojure.test]))
 
 
-(deftest can-split-an-array
-  (is (= (which-half? [1, 2, 3, 4] 1) '(1,2)))
-  (is (= (which-half? [1, 2, 3, 4] 2) '(1,2)))
-  (is (= (which-half? [1, 2, 3, 4, 5] 3) '(1,2,3)))
-  (is (= (which-half? [1, 3, 5, 67, 99] 7) '(67,99)))
-  (is (= (which-half? [1, 3, 5, 67, 99] 5) '(1,3,5)))
-    )
+;(deftest can-split-an-array
+;  (is (= (which-half? [1, 2, 3, 4] 1) :first_half))
+;  (is (= (which-half? [1, 2, 3, 4] 2) :first_half))
+;  (is (= (which-half? [1, 2, 3, 4, 5] 3) :first_half))
+;  (is (= (which-half? [1, 3, 5, 67, 99] 7) :second_half))
+;  (is (= (which-half? [1, 3, 5, 67, 99] 5) :first_half))
+;    )
 
 (deftest can-tell-whether-a-target-exist
-  (is (= (where-is-it? [1, 2, 3, 4] 1) :true))
-  (is (= (where-is-it? [1, 2, 3, 4] 4) :true))
-  (is (= (where-is-it? [1, 2, 3, 4] 5) nil))
+;  (is (= (where-is-it? [1, 2, 3, 4] 1) 0))
+  (is (= (where-is-it? [1, 2, 3, 4] 4) 3))
+;  (is (= (where-is-it? [1, 2, 3, 4] 5) nil))
   )
 
 (run-tests)
