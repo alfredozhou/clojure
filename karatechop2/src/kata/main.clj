7caba227d1cc36482b9bd723a772b5f628da4ae2
salsayarroz <salsayarroz@gmail.com>
2011-01-04 10:54:24 -0500

    finally made the binary search work

--- karatechop2/src/kata/main.clj
+++ karatechop2/src/kata/main.clj
@@ -1,44 +1,36 @@
 (ns kata.main
   (:use clojure.contrib.math))
 
-(defn first-half [array]
-  (take (/ (count array) 2) array)
+(defn middle-index [array]
+  (floor (/ (count array) 2))
   )
 
-(defn i-am-in-second-half []
-  (print "I am in second half\n")
-  :second_half
+(defn pivit-value [array]
+  (if (> (count array) 0) (nth array (middle-index array)))
   )
-
-(defn which-half? [array target]
-  (print "target=" target " sub list ="  array "\n")
-  (let [first-half (first-half array)]
-    (cond
-      (>= (last first-half) target) :first_half
-      :else (i-am-in-second-half))
-    ))
-
 
 (defn sub-array [array from to]
   (subvec array from to)
   )
 
-(defn calculate-index [to from]
-  (+ (/ to 2) (/ (- to from) 2))
+(defn which-half? [array target from to]
+  (let [sub-array (sub-array array from to)]
+    (let [pivit (pivit-value sub-array)]
+      (cond
+        (nil? pivit) nil
+        (> pivit target) :first_half
+        (= pivit target) (+ (middle-index sub-array) from)
+        :else :second_half))
+    )
   )
 
 (defn- private-where-is-it? [array target from-index to-index]
-  ;  (print "target = " target " list =" array " from " from-index " to " to-index "\n")
-  (cond
-    (and (= from-index to-index) (= (nth array to-index) target)) to-index
-    (and (= from-index to-index) (not= (nth array to-index) target)) nil
-    :else
-    (let [which-half? (which-half? (sub-array array from-index to-index) target)]
-      (cond
-        (= which-half? :first_half) (private-where-is-it? array target from-index (floor (/ to-index 2)))
-        :else (private-where-is-it? array target (calculate-index  to-index from-index) to-index)
-        ))
-    )
+  (let [which-half-is-it? (which-half? array target from-index to-index)]
+    (cond
+      (= which-half-is-it? :first_half) (private-where-is-it? array target from-index (floor (/ to-index 2)))
+      (= which-half-is-it? :second_half) (private-where-is-it? array target (+ from-index (floor (/ to-index 2))) to-index)
+      :else which-half-is-it?
+      ))
   )
 
 (defn where-is-it? [array target]
--- karatechop2/test/kata/test/main.clj
+++ karatechop2/test/kata/test/main.clj
@@ -3,18 +3,22 @@
   (:use [clojure.test]))
 
 
-;(deftest can-split-an-array
-;  (is (= (which-half? [1, 2, 3, 4] 1) :first_half))
-;  (is (= (which-half? [1, 2, 3, 4] 2) :first_half))
-;  (is (= (which-half? [1, 2, 3, 4, 5] 3) :first_half))
-;  (is (= (which-half? [1, 3, 5, 67, 99] 7) :second_half))
-;  (is (= (which-half? [1, 3, 5, 67, 99] 5) :first_half))
-;    )
+(deftest can-split-an-array
+  (is (= (which-half? [1, 2, 3, 4] 1 0 4) :first_half))
+  (is (= (which-half? [1, 2, 3, 4] 2 0 4) :first_half))
+  (is (= (which-half? [1, 2, 3, 4, 5] 3 0 4) 2))
+  (is (= (which-half? [1, 3, 5, 67, 99] 7 0 5) :second_half))
+  (is (= (which-half? [1, 3, 5, 67, 99] 5 0 5) 2))
+  )
 
 (deftest can-tell-whether-a-target-exist
-;  (is (= (where-is-it? [1, 2, 3, 4] 1) 0))
+  (is (= (where-is-it? [1, 2, 3, 4] 1) 0))
+  (is (= (where-is-it? [1, 2, 3, 4] 2) 1))
+  (is (= (where-is-it? [1, 2, 3, 4] 3) 2))
   (is (= (where-is-it? [1, 2, 3, 4] 4) 3))
-;  (is (= (where-is-it? [1, 2, 3, 4] 5) nil))
+  (is (= (where-is-it? [1, 2, 3, 4, 5] 5) 4))
+  (is (= (where-is-it? [0, 1, 2, 3, 4, 5, 6, 7, 8] 3) 3))
+  (is (nil? (where-is-it? [1, 2, 3, 4] 5)))
   )
 
 (run-tests)
